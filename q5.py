f946bc7ccb99f39a3a306b39b0d7ba9c76bce6e5
Ericoreto <111282811+Ericoreto@users.noreply.github.com>
2022-12-05 08:57:37 -0300

    Update q5.py

--- q5.py
+++ q5.py
@@ -52,37 +52,37 @@
 # Para obter a nota máxima dessa questão, não deve-se utilizar nenhuma função pronta do Python.
 
 def q5(s):
-    dict = {}
-    y = 0
-    def aab():
-        for n in range(0,len(senha)):
-            if tem(senha[n]) == False:
-                dict[senha[n]] = 0
-            else:
-                dict[senha[n]] = valor(senha[n]) +1
-        for i in range(0, len(dict)):
-            if valor(senha[i]) != valor(senha[len(dict)]) or bbb() = True:
-                return False
-        return True
-    def tem(aa):
-        for k in dict.keys():
-            if aa == k:
-                return True
-        return False
-    def valor(nome):
-        for k, v in dict.items():
-             if nome == k:
-                 return v
-    def bbb():
-        for i in dict(0, len(dict)):
-                if y < valor(senha[i]):
-                    y = valor(senha[i])
-                    yn = i
-            valor(senha[i]) -= 1
-        for i in range(0, len(dict))
-        if valor(senha[i]) != valor(senha[len(dict)]):
+        dict = {}
+        y = 0
+        def aab():
+            for n in range(0,len(senha)):
+                if tem(senha[n]) == False:
+                    dict[senha[n]] = 0
+                else:
+                    dict[senha[n]] = valor(senha[n]) +1
+            for i in range(0, len(dict)):
+                if valor(senha[i]) != valor(senha[len(dict)]) or bbb() = True:
+                    return False
+            return True
+        def tem(aa):
+            for k in dict.keys():
+                if aa == k:
+                    return True
             return False
-        return True
+        def valor(nome):
+            for k, v in dict.items():
+                 if nome == k:
+                     return v
+        def bbb():
+            for i in dict(0, len(dict)):
+                    if y < valor(senha[i]):
+                        y = valor(senha[i])
+                        yn = i
+                valor(senha[i]) -= 1
+            for i in range(0, len(dict))
+            if valor(senha[i]) != valor(senha[len(dict)]):
+                return False
+            return True
     return aab()
 
 
