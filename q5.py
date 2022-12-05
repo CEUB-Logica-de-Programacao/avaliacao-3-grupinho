af86fec9371133fd33f97b27d95b4e161fc1afe5
DigogSXD <111078132+DigogSXD@users.noreply.github.com>
2022-12-05 08:26:49 -0300

    Update q5.py

--- q5.py
+++ q5.py
@@ -56,5 +56,34 @@ def q5(s):
     return False
 
 
+
+dict = {}
+def aab():
+    for n in range(0,len(senha)):
+        if tem(senha[n]) == False:
+            dict[senha[n]] = 0
+        else:
+            dict[senha[n]] = valor(senha[n]) +2
+    for i in range(0, len(dict)):
+        if valor(senha[i]) != valor(senha[len(dict)]):
+            return False
+    return True
+def tem(aa):
+    for k in dict.keys():
+        if aa == k:
+            return True
+    return False
+def valor(nome):
+    for k, v in dict.items():
+         if nome == k:
+             return v
+print(f'Sua senha é {senha}')
+senha = input('Digite sua senha')
+while aab() == False:
+    print('Senha inválida')
+    break
+
+
+
 if __name__ == '__main__':
     print(q5('abcc'))
