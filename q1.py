f96e9fa2fee8c435907ef3d28ee4d1edfef778e2
Ericoreto <111282811+Ericoreto@users.noreply.github.com>
2022-12-05 09:12:12 -0300

    Update q1.py

--- q1.py
+++ q1.py
@@ -27,7 +27,7 @@ def q1(heights):
     y = 1
     for i in range(0,len(heights)):
         if heights[i] >x:
-            x = heights[i]
+            x == heights[i]
             y = 1
         if heights[i] = x:
             y +=1
