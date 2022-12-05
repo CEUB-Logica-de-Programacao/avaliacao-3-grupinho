978a2e056dc15c63c53f5e38df5be5a09e211de2
Ericoreto <111282811+Ericoreto@users.noreply.github.com>
2022-12-05 08:29:14 -0300

    Update q5.py

--- q5.py
+++ q5.py
@@ -57,29 +57,29 @@ def q5(s):
 
 
 
-dict = {}
-def aab():
-    for n in range(0,len(senha)):
-        if tem(senha[n]) == False:
-            dict[senha[n]] = 0
-        else:
-            dict[senha[n]] = valor(senha[n]) +2
-    for i in range(0, len(dict)):
-        if valor(senha[i]) != valor(senha[len(dict)]):
-            return False
-    return True
-def tem(aa):
-    for k in dict.keys():
-        if aa == k:
-            return True
-    return False
-def valor(nome):
-    for k, v in dict.items():
-         if nome == k:
-             return v
-print(f'Sua senha é {senha}')
-senha = input('Digite sua senha')
-while aab() == False:
+    dict = {}
+    def aab():
+        for n in range(0,len(senha)):
+            if tem(senha[n]) == False:
+                dict[senha[n]] = 0
+            else:
+                dict[senha[n]] = valor(senha[n]) +2
+        for i in range(0, len(dict)):
+            if valor(senha[i]) != valor(senha[len(dict)]):
+                return False
+        return True
+    def tem(aa):
+        for k in dict.keys():
+            if aa == k:
+                return True
+        return False
+    def valor(nome):
+        for k, v in dict.items():
+             if nome == k:
+                 return v
+    print(f'Sua senha é {senha}')
+    senha = input('Digite sua senha')
+    while aab() == False:
     print('Senha inválida')
     break
 
